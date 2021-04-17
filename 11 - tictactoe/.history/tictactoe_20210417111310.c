dc0b35d1f75aacf8911063c9257e7cd76bc6b686
Damian Demasi <damian.demasi.1@gmail.com>
2021-04-17 11:46:32 +0930

    drawBoard function

---
+++ 11 - tictactoe/.history/tictactoe_20210417111310.c
@@ -0,0 +1,20 @@
+/*************************
+ * By Damian Demasi
+ * Date: 17/04/2021
+ * Requirements:
+ *  Write a program that plays Tic Tac Toe. The game is played on a 3x3 grid, by 2 players who take turns.
+ *  Use three functions:
+ *      - Check to see if a player won or the game is draw.
+ *      - Redraw the board for each player turn.
+ *      - Set a char array with a selection and check for an invalid selection.
+ *************************/
+
+#include <stdio.h>
+
+
+int main()
+{
+
+    return 0;
+}
+
---
+++ 11 - tictactoe/.history/tictactoe_20210417113618.c
@@ -0,0 +1,33 @@
+/*************************
+ * By Damian Demasi
+ * Date: 17/04/2021
+ * Requirements:
+ *  Write a program that plays Tic Tac Toe. The game is played on a 3x3 grid, by 2 players who take turns.
+ *  Use three functions:
+ *      - Check to see if a player won or the game is draw.
+ *      - Redraw the board for each player turn.
+ *      - Set a char array with a selection and check for an invalid selection.
+ *************************/
+
+#include <stdio.h>
+
+// Function declarations
+void drawBoard();
+
+// Function definitions
+int main()
+{
+    drawBoard();
+
+    return 0;
+}
+
+void drawBoard()
+{
+    printf("\n\t  _____ _        _____            _____           ");
+    printf("\n\t |_   _(_) ___  |_   _|_ _  ___  |_   _|__   ___  ");
+    printf("\n\t   | | | |/ __|   | |/ _` |/ __|   | |/ _ \\ / _ \\ ");
+    printf("\n\t   | | | | (__    | | (_| | (__    | | (_) |  __/ ");
+    printf("\n\t   |_| |_|\\___|   |_|\\__,_|\\___|   |_|\\___/ \\___| ");
+
+}
